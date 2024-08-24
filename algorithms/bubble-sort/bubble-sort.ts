0c746c23a8819220a21fbc554cbd02c6b5d30ef3
ZhangTerrence <terrencezhang77@gmail.com>
2024-08-24 17:57:13 -0400

    chore: added prettier

--- algorithms/bubble-sort/bubble-sort.ts
+++ algorithms/bubble-sort/bubble-sort.ts
@@ -8,41 +8,41 @@ const array1DTracer = new Array1DTracer("Array");
 const logTracer = new LogTracer("Log");
 
 function BubbleSort(array: Array<number>): void {
-    const n = array.length;
+  const n = array.length;
 
-    logTracer.print(`initial array - [${array.join(", ")}]`);
-    array1DTracer.captureState();
+  logTracer.print(`initial array - [${array.join(", ")}]`);
+  array1DTracer.captureState();
 
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            logTracer.print("comparing indexes j and j + 1");
-            array1DTracer.select([j, j + 1], {
-                i: i,
-                j: j,
-            });
-            if (array[j] > array[j + 1]) {
-                logTracer.print("swapping values at indexes j and j + 1");
-                array1DTracer.swap(j, j + 1, {
-                    i: i,
-                    j: j
-                });
-                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      logTracer.print("comparing indexes j and j + 1");
+      array1DTracer.select([j, j + 1], {
+        i: i,
+        j: j,
+      });
+      if (array[j] > array[j + 1]) {
+        logTracer.print("swapping values at indexes j and j + 1");
+        array1DTracer.swap(j, j + 1, {
+          i: i,
+          j: j,
+        });
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
 
-                logTracer.print(`updated array - [${array.join(", ")}]`);
-                array1DTracer.captureState({
-                    i: i,
-                    j: j,
-                });
-            }
-        }
+        logTracer.print(`updated array - [${array.join(", ")}]`);
+        array1DTracer.captureState({
+          i: i,
+          j: j,
+        });
+      }
     }
+  }
 
-    logTracer.print(`sorted array - [${array.join(", ")}]`);
-    array1DTracer.captureState();
+  logTracer.print(`sorted array - [${array.join(", ")}]`);
+  array1DTracer.captureState();
 }
 
 (function main() {
-    array1DTracer.setArray((new Array1DRandomizer(10, 0, 10)).getArray());
-    BubbleSort(array);
-    (new Scheduler(array1DTracer, logTracer)).run();
+  array1DTracer.setArray(new Array1DRandomizer(10, 0, 10).getArray());
+  BubbleSort(array);
+  new Scheduler(array1DTracer, logTracer).run();
 })();
--- algorithms/selection-sort/selection-sort.ts
+++ algorithms/selection-sort/selection-sort.ts
@@ -8,53 +8,53 @@ const array1DTracer = new Array1DTracer("Array");
 const logTracer = new LogTracer("Log");
 
 function SelectionSort(array: Array<number>): void {
-    const n = array.length;
+  const n = array.length;
 
-    logTracer.print(`initial array - [${array.join(", ")}]`);
-    array1DTracer.captureState();
+  logTracer.print(`initial array - [${array.join(", ")}]`);
+  array1DTracer.captureState();
 
-    for (let i = 0; i < n; i++) {
-        let minIndex = i;
+  for (let i = 0; i < n; i++) {
+    let minIndex = i;
 
-        for (let j = i + 1; j < n; j++) {
-            logTracer.print("comparing indexes j and minIndex");
-            array1DTracer.select([j, j + 1], {
-                i: i,
-                j: j,
-                minIndex: minIndex
-            });
-            if (array[j] < array[minIndex]) {
-                logTracer.print("setting index minIndex to index j");
-                array1DTracer.captureState({
-                    i: i,
-                    j: j,
-                    minIndex: j,
-                });
-                minIndex = j;
-            }
-        }
-
-        logTracer.print("swapping values at indexes i and minIndex");
-        array1DTracer.swap(i, minIndex, {
-            i: i,
-            j: j,
-            minIndex: minIndex,
-        });
-        [array[i], array[minIndex]] = [array[minIndex], array[i]];
-
-        logTracer.print(`updated array - [${array.join(", ")}]`);
+    for (let j = i + 1; j < n; j++) {
+      logTracer.print("comparing indexes j and minIndex");
+      array1DTracer.select([j, j + 1], {
+        i: i,
+        j: j,
+        minIndex: minIndex,
+      });
+      if (array[j] < array[minIndex]) {
+        logTracer.print("setting index minIndex to index j");
         array1DTracer.captureState({
-            i: i,
-            j: j,
+          i: i,
+          j: j,
+          minIndex: j,
         });
+        minIndex = j;
+      }
     }
 
-    logTracer.print(`sorted array - [${array.join(", ")}]`);
-    array1DTracer.captureState();
+    logTracer.print("swapping values at indexes i and minIndex");
+    array1DTracer.swap(i, minIndex, {
+      i: i,
+      j: j,
+      minIndex: minIndex,
+    });
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+
+    logTracer.print(`updated array - [${array.join(", ")}]`);
+    array1DTracer.captureState({
+      i: i,
+      j: j,
+    });
+  }
+
+  logTracer.print(`sorted array - [${array.join(", ")}]`);
+  array1DTracer.captureState();
 }
 
 (function main() {
-    array1DTracer.setArray(new Array1DRandomizer(10, 0, 10).getArray());
-    SelectionSort(array);
-    (new Scheduler(array1DTracer, logTracer)).run();
+  array1DTracer.setArray(new Array1DRandomizer(10, 0, 10).getArray());
+  SelectionSort(array);
+  new Scheduler(array1DTracer, logTracer).run();
 })();
